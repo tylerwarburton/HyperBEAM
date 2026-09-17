52b7b037b7789750becc3eb45db87d4c0110910b
Tyler Warburton <tyler@cipherplay.net>
2026-09-17 14:19:37 -0400

    perf(process): stop sizing every computed state

--- src/preloaded/process/dev_process.erl
+++ src/preloaded/process/dev_process.erl
@@ -425,7 +425,6 @@ compute_slot(ProcID, State, RawInputMsg, InitReq, TargetSlot, Opts) ->
                     {prep_ms, PrepTimeMicroSecs div 1000},
                     {execution_ms, RuntimeMicroSecs div 1000},
                     {store_ms, StoreTimeMicroSecs div 1000},
-                    {computed_slot_size, erlang:external_size(NewProcStateMsgWithSlot)},
                     {action,
                         hb_ao:get(
                             <<"body/action">>,
