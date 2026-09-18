2e4d851f726e1ba56e67f43b6b616a2af4e463f7
Tyler Warburton <tyler@cipherplay.net>
2026-09-17 21:15:09 -0400

    Revert "test(process-worker): derive the process cache key"
    This reverts commit d908a318206b5e070c5eff3c0c195eea939f822c.

--- src/preloaded/process/dev_process_worker.erl
+++ src/preloaded/process/dev_process_worker.erl
@@ -468,7 +468,7 @@ worker_survives_forced_message_slot_test_() ->
                 <<"spawn-worker">> => false,
                 <<"process-workers">> => false
             },
-        Group = process_to_group_name(Base, Opts),
+        Group = hb_util:encode(crypto:strong_rand_bytes(32)),
         Self = self(),
         Worker = spawn(fun() -> server(Group, Base, WorkerOpts) end),
         MRef = erlang:monitor(process, Worker),
